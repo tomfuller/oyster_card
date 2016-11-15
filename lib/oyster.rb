01d49df519283c7d98d0903015f1615475ad0308
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-15 16:52:23 +0000

    saved exit touch out

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,11 +1,13 @@
 class Oyster
-attr_reader :balance, :in_journey, :entry_station
+attr_reader :balance, :in_journey, :entry_station, :exit_station, :journeys
 DEFAULT_BALANCE = 0
 MAX_CAPACITY = 90
 MINIMUM_FARE = 1
  def initialize(balance = DEFAULT_BALANCE)
    @balance = balance
-    @entry_station = nil
+   @entry_station = nil
+   @exit_station = nil
+   @journeys = {}
  end
 
 def top_up(money)
@@ -17,16 +19,26 @@ end
 def touch_in(station)
   message = "You're poor, go and top up"
   fail message if @balance < MINIMUM_FARE
-  save(station)
+  save_entry(station)
 end
 
-def touch_out
+def touch_out(station)
   deduct
   delete_entry
+  save_exit(station)
+  save_journey
 end
 
-def save(station)
+def save_journey
+  @journey[entry_station] = exit_station
+end
+
+def save_entry(station)
   @entry_station = station
+end
+
+def save_exit(station)
+  @exit_station = station
 end
 
 private
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -6,6 +6,8 @@ require 'oyster'
 describe Oyster do
   subject(:card) {described_class.new}
   let(:station) {double(:station)}
+  let(:station2) {double(:station)}
+
   describe "Balance" do
     it "should display zero when object is initialized" do
       expect(card.balance).to eq 0
@@ -46,39 +48,53 @@ describe Oyster do
     end
 
     it "should return nil when touching out" do
-      card.touch_out
+      card.touch_out(station)
       expect(card.entry_station).to eq nil
     end
 
     it "should deduct minimum fare from the balance when checking out" do
       card.touch_in(station)
-      expect{card.touch_out}.to change{card.balance}.by(-1)
+      expect{card.touch_out(station)}.to change{card.balance}.by(-1)
     end
 
   end
 
   describe "To keep history of journeys" do
     before(:each) {card.top_up(10)}
-    it "should return the last station were we touched in" do
-      expect(card.save(station)).to eq station
+    it "should return the station were we touched in" do
+      expect(card.save_entry(station)).to eq station
+    end
+
+    it "should return the station where we touched out at" do
+      expect(card.save_exit(station)).to eq station
     end
 
     it "should take station as a variable when touching in" do
       expect(card).to respond_to(:touch_in).with(1).argument
     end
 
-    it "should save the last station" do
+    it "should save the entry station" do
 
       card.touch_in(station)
       expect(card.entry_station).to eq station
     end
 
+    it "should save the exit station" do
+      card.touch_out(station)
+      expect(card.exit_station).to eq station
+    end
+
     it "should forget the entry station after check out" do
       card.touch_in(station)
-      card.touch_out
+      card.touch_out(station)
       expect(card.entry_station).to eq nil
     end
 
+    #it "should store a journey" do
+    #  card.touch_in(station)
+    #  card.touch_out(station2)
+    #  expect(card.journeys[station]).to eq station2
+    #end
   end
 
 end
