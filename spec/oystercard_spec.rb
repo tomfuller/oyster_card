1fe45fcb0ffedb8383d5743552075c053c4a51dd
Mike Hurl <mikehurl@gmail.com>
2016-11-15 13:05:25 +0000

    adds code to top up balance of card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,11 +3,20 @@ require "oystercard"
 describe Oystercard do
 
 it "should respond to balance" do
-expect(subject).to respond_to(:balance)
+  expect(subject).to respond_to(:balance)
 end
 
 it "should expect the balance to equal zero for a new card" do
-expect(subject.balance).to eq(0)
+  expect(subject.balance).to eq(0)
+end
+
+it "should respond to 'top_up'" do
+  expect(subject).to respond_to(:top_up)
+end
+
+it "should expect the balance to increase when card is topped up" do
+  subject.top_up(10)
+  expect(subject.balance).to eq 10
 end
 
 end
