12a20a1dad6378a6a6ba46c92379a38575a950e6
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-15 13:01:35 +0000

    added balance functions

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,20 @@
 class Oystercard
 
+  DEFAULT_VALUE = 0
+  MAXIMUM_BALANCE = 90
+  attr_accessor :balance
+
+
+  def initialize
+    @balance = DEFAULT_VALUE
+  end
+
+  def top_up(value)
+    raise "Maximum balance limit of £#{MAXIMUM_BALANCE} exceeded" if (balance + value) > MAXIMUM_BALANCE
+    self.balance += value
+  end
+
+  def deduct(value)
+    self.balance -= value
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,23 @@
 require 'oystercard'
 describe Oystercard do
 
+  it 'should test that oystercard is initialized with a balance of 0' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'should top up value of oyster card with given value' do
+    subject.top_up(30)
+    expect(subject.balance).to eq 30
+  end
+
+  it 'should raise an error if maximun balance is exceeded' do
+    expect{subject.top_up(95)}.to raise_error "Maximum balance limit of £90 exceeded"
+  end
+
+  it 'should be able to deduct a given value from the balance' do
+    subject.top_up(40)
+    subject.deduct(30)
+    expect(subject.balance).to eq 10
+  end
+
 end
