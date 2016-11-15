6be38cb6264f48132b38bb4a9a9bbed5546f7175
Simon <simon.conway@condaal.com>
2016-11-15 12:43:05 +0000

    Added a balance to a card and set at 0 by default

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require "oystercard"
 
 describe Oystercard do
+
+it "should respond to balance" do
+expect(subject).to respond_to(:balance)
+end
+
+it "should expect the balance to equal zero for a new card" do
+expect(subject.balance).to eq(0)
+end
+
 end
