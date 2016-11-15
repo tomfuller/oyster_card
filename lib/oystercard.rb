10a3590ef5ac6ae83544adf13b5e429614868d81
Simon <simon.conway@condaal.com>
2016-11-15 14:39:21 +0000

    Enforced a maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,15 @@ class Oystercard
 
 attr_reader :balance
 
+MAXIMUM_BALANCE = 90
+
 def initialize
   @balance = 0
 end
 
 def top_up(amount)
+  fail "Top up too much. Maximum balance is £90. Current
+  balance is #{self.balance}" if @balance + amount > MAXIMUM_BALANCE
   @balance += amount
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -19,4 +19,9 @@ it "should expect the balance to increase when card is topped up" do
   expect(subject.balance).to eq 10
 end
 
+it "should raise an error when a top up takes the balance over 90" do
+  subject.top_up(Oystercard::MAXIMUM_BALANCE)
+  expect { subject.top_up(1) }.to raise_error("Top up too much. Maximum balance is £90. Current
+  balance is #{subject.balance}")
+end
 end
