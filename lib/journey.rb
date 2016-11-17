e882ff90ef20d10c598d1e269fd55c69df581e71
Chris <chrixs99@hotmail.com>
2016-11-17 11:45:04 +0000

    added fare calculation functionality

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,8 @@
 class Journey
 
   attr_accessor :start_journey, :end_journey
+  PENALTY_FARE = 6
+  MINIMUM_FARE = 1
 
   def initialize
     @start_journey = false
@@ -15,5 +17,13 @@ class Journey
     self.end_journey = true
   end
 
+  def journey_complete?
+    self.start_journey == self.end_journey
+  end
+
+  def calculate_fare
+    return PENALTY_FARE unless journey_complete?
+    MINIMUM_FARE
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -16,15 +16,41 @@ describe Journey do
 
   end
 
-  it 'should test that journey_start method changes start_journey to true' do
-    journey.journey_start
-    expect(journey.start_journey).to eq true
+  context 'start, end and complete journey tests' do
+
+    it 'should test that journey_start method changes start_journey to true' do
+      journey.journey_start
+      expect(journey.start_journey).to eq true
+    end
+
+    it 'should test that journey_end method changes end_journey to true' do
+      journey.journey_end
+      expect(journey.end_journey).to eq true
+    end
+
+    it 'should test that a journey has been completed' do
+      journey.journey_start
+      journey.journey_end
+      expect(journey.journey_complete?).to eq true
+    end
+
   end
 
-  it 'should test that journey_end method changes end_journey to true' do
-    journey.journey_end
-    expect(journey.end_journey).to eq true
-  end 
+  context 'fare calculation' do
+
+    it "should return #{Journey::MINIMUM_FARE} when journey is completed" do
+      journey.journey_start
+      journey.journey_end
+      expect(journey.calculate_fare).to eq (1)
+    end
+
+    it "should return #{Journey::PENALTY_FARE} when journey is incomplete" do
+      journey.journey_start
+      expect(journey.calculate_fare).to eq (6)
+    end
+
+  end
+
 
 
 
