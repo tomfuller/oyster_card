bbadae273bcd729bfda83779a9968e524fc9f140
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-18 11:09:46 +0000

    code refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,6 @@
 class Oystercard
 
-attr_reader :balance, :entry_station, :journey_history
+attr_accessor :balance, :entry_station, :journey_history, :journey_counter
 
 MAXIMUM_BALANCE = 90
 MINIMUM_BALANCE = 1
@@ -13,42 +13,42 @@ def initialize
 end
 
 def top_up(amount)
-  fail "Top up too much. Maximum balance is £90. Current balance is #{@balance}" if @balance + amount > MAXIMUM_BALANCE
-  @balance += amount
+  fail "Top up too much. Maximum balance is £90. Current balance is #{self.balance}" if self.balance + amount > MAXIMUM_BALANCE
+  self.balance += amount
 end
 
 def in_journey?
-  !!@entry_station
+  !!self.entry_station
 end
 
 def touch_in(station)
-  if @balance < MINIMUM_BALANCE
+  if self.balance < MINIMUM_BALANCE
     fail("You do not have sufficient funds. Please top up your card.")
   else
-    @entry_station = station
+    self.entry_station = station
     record_entry(station)
   end
 end
 
 def touch_out(station)
   record_exit(station)
-  @entry_station = nil
+  self.entry_station = nil
   deduct
 end
 
 def record_entry(station)
-  @journey_history["journey_#{@journey_counter}"] = { "entry" => station}
+  self.journey_history["journey_#{self.journey_counter}"] = { "entry" => station}
 end
 
 def record_exit(station)
-  @journey_history["journey_#{@journey_counter}"].merge!({ "exit" => station})
-  @journey_counter += 1
+  self.journey_history["journey_#{self.journey_counter}"].merge!({ "exit" => station})
+  self.journey_counter += 1
 end
 
 private
 
 def deduct
-  @balance -= MINIMUM_BALANCE
+  self.balance -= MINIMUM_BALANCE
 end
 
 
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,16 +2,15 @@ require './lib/station.rb'
 
 describe Station do
 
-before(:each) do
-  @hammersmith = Station.new("Hammersmith", "Zone 2")
-end
+let(:hammersmith) { described_class.new("Hammersmith", "Zone 2")}
+
 
   it "will be able to show the station name" do
-    expect(@hammersmith.name).to eq("Hammersmith")
+    expect(hammersmith.name).to eq("Hammersmith")
   end
 
   it "will be able to show the station's zone" do
-    expect(@hammersmith.zone).to eq("Zone 2")
+    expect(hammersmith.zone).to eq("Zone 2")
   end
 
 
